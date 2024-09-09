62a4e98a120bdf5aecc5f27e2f3153c1c05fff2a
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-09 15:56:09 -0300

    Usuario logando

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ChatController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,5 +11,13 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 });
 
-Route::middleware('auth:api')->post('/send-message', [ChatController::class, 'sendMessage']);
+//Route::middleware('auth:api')->post('/send-message', [ChatController::class, 'sendMessage']);
 
+
+Route::Post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/validate-token', [AuthController::class, 'validateToken']);
+
+    Route::post('/send-message', [ChatController::class, 'sendMessage']);
+});
