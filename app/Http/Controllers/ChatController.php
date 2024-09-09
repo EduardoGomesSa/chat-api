6acf9036c1e83e667f9f89fa6dad75b46300d2ff
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-08 23:50:05 -0300

    Configuração do método sendMessage no ChatController

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\MessageSent;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
-    //
+    public function sendMessage(Request $request){
+        $request->validate([
+            'message' => 'required|string',
+            'receiver_id' => 'required|in|exists:users,id',
+        ]);
+
+        $message = $request->message;
+        $user = auth()->user;
+
+        broadcast(new MessageSent($message, $user))->toOthers();
+
+        return response()->json(['status'=>'Message Sent!']);
+    }
 }
